7a03ea5174035d14a9965c3009bc0774619443d7
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-14 17:57:04 -0300

    Create TokenRefresh resource

--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,10 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token, 
+    create_refresh_token, 
+    jwt_refresh_token_required, 
+    get_jwt_identity
+)
 from werkzeug.security import safe_str_cmp
 
 from models.user import UserModel
@@ -7,6 +12,7 @@ from models.user import UserModel
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument("username", type=str, required=True, help="This field cannot be left blank!")
 _user_parser.add_argument("password", type=str, required=True, help="This field cannot be left blank!")
+
 
 class UserRegister(Resource):
     def post(self):
@@ -19,6 +25,7 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return {"message": "User created sucessfully."}, 201
+
 
 class User(Resource):
     def get(self, user_id):
@@ -34,6 +41,7 @@ class User(Resource):
         user.delete_from_db()
         return {"message": "User deleted."}, 200
 
+
 class UserLogin(Resource):
     def post(self):
         data = _user_parser.parse_args()
@@ -48,3 +56,11 @@ class UserLogin(Resource):
                 "refresh_token": refresh_token,
             }, 200
         return {"message": "Invalid credentials."}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}, 200
