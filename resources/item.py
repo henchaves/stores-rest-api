1d54e4b261b01cdfdfcd84b613cfc70f44df974d
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-14 18:03:46 -0300

    Require a fresh token in an endpoint

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
+from flask_jwt_extended import (
+    jwt_required, 
+    fresh_jwt_required, 
+    get_jwt_claims, 
+    jwt_optional, 
+    get_jwt_identity
+)
 from models.item import ItemModel
 
 
@@ -25,6 +31,7 @@ class Item(Resource):
             return item.json()
         return {"message": "Item not found."}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return (
